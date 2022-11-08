042bdfd75227590fb4e1e12f00f39e1c8e32e07b
ganeshIIT <21f1002245@student.onlinedegree.iitm.ac.in>
2022-11-08 17:44:04 +0530

    added exploration sql file

--- exploration.sql
+++ exploration.sql
@@ -1,41 +1,38 @@
-use airbnb
-go
+select top 6 * from listings(nolock)
+select top 6 * from calendar(nolock)
+select top 6 * from reviews(nolock) 
 
-alter procedure stats_ReviewsAndPrice
-(
-@country varchar(150) = 'all',
-@province varchar(150) = 'all',
-@city varchar(150) = 'all'
-)
-as
--- exec stats_ReviewsAndPrice @province ='tx'
--- declare @city varchar(150) = 'all'
--- declare @country varchar(150) = 'all'
--- declare @province varchar(150) = 'all'
--- set @city = 'austin'
--- set @country = 'united-states'
--- set @province = 'tx'
 
-select l.listing_id, 
-country, 
-province,city, 
-case when l.availability_365 > 0 then 1 else 0 end as isActive,
-l.host_since,
-l.review_scores_accuracy,
-l.review_scores_checkin,
-l.review_scores_cleanliness,
-l.review_scores_communication,
-l.review_scores_communication,
-l.review_scores_location,
-l.review_scores_rating,
-l.review_scores_value,
-l.number_of_reviews,
-l.price,  
-[c].[Price] as CurrentPrice 
-from listings l 
-left join (select listing_id, avg(price) as Price 
-from calendar 
-group by listing_id) c on c.listing_id = l.listing_id
-where (l.city = @city or 'all' = @city)
-and (l.country = @country or 'all' = @country)
-and (l.province = @province or 'all' = @province)
+select count(*) from listings(nolock)
+select count(*) from reviews(nolock)
+select count(*) from calendar(nolock)
+
+
+select top 100 c.*, l.host_name from listings l(nolock)
+join calendar c (nolock) on c.listing_id = l.listing_id
+
+select * from listings where len(listing_id) = 18
+
+
+
+-- ALTER TABLE listings alter column listing_id varchar(24)
+
+-- ALTER TABLE listings  
+-- ADD CONSTRAINT unique_listing_id UNIQUE (listing_id);  
+
+
+select c.*, l.city, l.host_name, l.number_of_reviews, l.review_scores_rating 
+from listings l(nolock)
+join calendar c (nolock) on c.listing_id = l.listing_id
+where c.date = '2022-10-12'
+and c.available = 1
+and l.city like 'new%york%'
+and number_of_reviews > 50 and review_scores_rating > 4.5
+order by review_scores_rating desc, price asc
+
+
+select distinct(province) from listings(nolock)
+
+
+-- create proc test as 
+-- select top 10 * from calendar
